bc724e2e8d3c0f34eefc0ce408d56613c9b1d6e6
RykerConstance <65290499+RykerConstance@users.noreply.github.com>
2020-05-13 14:22:53 +0100

    Web Development folder
    This shows work i have done in things like HTML, CSS and JAVA

---
+++ Web Develpmont/Java/annoy.js
@@ -0,0 +1,18 @@
+// var answer = prompt("Are we there yet?");
+
+// while (answer !== "yes"){
+//    var answer = prompt("Are we there yet?");
+//}
+
+// alert("Yay we made it");
+
+// Version 2
+
+var answer = prompt("Are we there yet?");
+
+while (answer.indexOf("yes") === -1) {
+    var answer = prompt("Are we there yet?");
+}
+
+alert("Yay we made it");
+
